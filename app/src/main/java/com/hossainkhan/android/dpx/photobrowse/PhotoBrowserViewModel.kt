874325de866cbdb77a9e7fa340efaf28d94d219e
Hossain Khan <amardeshbd@gmail.com>
2019-03-01 13:53:11 -0500

    [ADDED] Live data based update from viewmodel.

--- app/src/main/java/com/hossainkhan/android/dpx/photobrowse/PhotoBrowserViewModel.kt
+++ app/src/main/java/com/hossainkhan/android/dpx/photobrowse/PhotoBrowserViewModel.kt
@@ -1,17 +1,31 @@
 package com.hossainkhan.android.dpx.photobrowse
 
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
 import com.hossainkhan.android.dpx.BuildConfig
 import com.hossainkhan.android.dpx.network.DpxApi
 import com.hossainkhan.android.dpx.network.models.Photos
-import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class PhotoBrowserViewModel(private val api: DpxApi) : ViewModel() {
+    // TODO - data binding not working with observable field.
+    val isNetworkRequestInProgress = ObservableField(false)
 
-    fun photoList(): Single<List<String>> {
-        return api.photos(Photos.FEATURE_POPULAR, BuildConfig.API_KEY)
-            .map { photos ->
-                photos.photos.map { it.name }
-            }
-    }
+    val photos: LiveData<List<String>>
+        get() {
+            return LiveDataReactiveStreams.fromPublisher(
+                api.photos(Photos.FEATURE_POPULAR, BuildConfig.API_KEY)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe { isNetworkRequestInProgress.set(true) }
+                    .doOnSuccess { isNetworkRequestInProgress.set(false) }
+                    .doOnError { isNetworkRequestInProgress.set(false) }
+                    .map { photos ->
+                        photos.photos.map { it.name }
+                    }.toFlowable()
+            )
+        }
 }
